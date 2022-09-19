24dd0bed6e4535420887fed0256b2a9a0ff5fe67
Sergio Moreno <smoreno@bch.bancodechile.cl>
2022-09-19 17:12:53 -0500

    se adiciona conexion a la base de datos

--- app/connection/db.py
+++ app/connection/db.py
@@ -1,17 +1,19 @@
 import os
 from dotenv import dotenv_values
 from sqlalchemy import create_engine
+import psycopg2
 
 
 def connection():
-  config = dotenv_values(".env") 
-  dictionary_config = dict(config)
-  host = dictionary_config["PG_HOST_"]
-  port = dictionary_config["PG_PORT_"]
-  user = dictionary_config["PG_USERNAME_"]
-  password = dictionary_config["PG_PASSWORD_"]
-  db = dictionary_config["PG_DB_"]
-  conn_string = 'postgresql://{}:{}@{}:{}/{}'.format(user, password,host,port,db)
-  db = create_engine(conn_string)
-  conn = db.connect()
-  return ["connection", conn]
+    config = dotenv_values(".env") 
+    dictionary_config = dict(config)
+    host = dictionary_config["PG_HOST_"]
+    port = dictionary_config["PG_PORT_"]
+    user = dictionary_config["PG_USERNAME_"]
+    password = dictionary_config["PG_PASSWORD_"]
+    db = dictionary_config["PG_DB_"]
+    conn_string = 'postgresql://{}:{}@{}:{}/{}'.format(user, password,host,port,db)
+    db = create_engine(conn_string)
+    conn = db.connect()
+    connection = psycopg2.connect(conn_string)
+    return [connection, conn]
